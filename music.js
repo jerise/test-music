e91fe40b0bb12b0ebdb1db3eb931d80d41c63857
jerise <38836954+jerise@users.noreply.github.com>
2019-02-12 21:12:32 +0200

    Update music.js

--- music.js
+++ music.js
@@ -7,7 +7,6 @@ const fetchVideoInfo = require("youtube-info");
 const botversion = require('./package.json').version;
 const simpleytapi = require('simple-youtube-api')
 const youtube = new simpleytapi(YT_API_KEY);
-client.login(TOKEN);
 var guilds = {};
 
 /////////////////////////////////////////////////////////////////
@@ -15,6 +14,30 @@ client.on('ready', () => client.user.setActivity(`${prefix}play , ${prefix}searc
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
  
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
 client.on('message', async function(message) {
     if(message.author.bot) return;
     if(!message.channel.guild) return;
@@ -634,3 +657,6 @@ function isYoutube(str) {
 }
 
 ////////////////
+
+
+client.login(process.env.BOT_TOKEN);
