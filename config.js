671fd7fcef06d28aab78b0bb599f25cc5b22dbd7
jerise <38836954+jerise@users.noreply.github.com>
2019-02-12 21:00:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    TOKEN: 'Your bot token here', 
-    YT_API_KEY: 'Youtube API Key here', 
-    prefix: 'prefix here',
-    devs: ['your id/developers here']
+    TOKEN: 'NTMyMDYzODM5MTczNDc2MzUy.D0So9Q.Mjgbd5a0kboDRbDyoRbSdmGBX30', 
+    YT_API_KEY: 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8', 
+    prefix: '#',
+    devs: ['448444251504640012']
 }
